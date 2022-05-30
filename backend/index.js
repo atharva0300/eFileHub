2dacea873d39486d45f087dfd71ebcd0a64bde8c
= <=>
2022-05-30 10:46:38 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+// imorting the modules 
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT  , () => {
+    console.log('Server listening on port number : ' , PORT);
+})
